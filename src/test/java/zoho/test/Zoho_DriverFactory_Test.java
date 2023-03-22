86f0d5caaad1e0fde1d510c62e2380513953d4b5
anilchander <tortes.steeper.0x@icloud.com>
2023-03-22 17:00:51 +0530

    google tests working fine.

--- src/test/java/zoho/test/Zoho_DriverFactory_Test.java
+++ src/test/java/zoho/test/Zoho_DriverFactory_Test.java
@@ -33,48 +33,48 @@ public class Zoho_DriverFactory_Test extends BaseTest {
 //		
 //	}
 	
-	@Test()
-	public void mytest2() throws Exception {
-		ZohoLogger logger = new ZohoLogger(this);
-		ZohoLandingPO landingPage = new ZohoLandingPO(this.getDriver());
-		logger.logInfo("Navigating to the landing Page...");
-		landingPage.navigate();
-		logger.logInfo("Clicking on the signin link");
-		ZohoSignInPO signInPage = landingPage.zohoSignIn();
-		ZohoHomePO homePage = signInPage.signInWithEmailAndPassword("hpamitsinha@gmail.com", "Avyaan@2023");
-		homePage.signOut();
-	}
-	
-	@Test()
-	public void mytest3() throws Exception {
-		ZohoLogger logger = new ZohoLogger(this);
-		ZohoLandingPO landingPage = new ZohoLandingPO(this.getDriver());
-		logger.logInfo("Navigating to the landing Page...");
-		landingPage.navigate();
-		logger.logInfo("Clicking on the signin link");
-		ZohoSignInPO signInPage = landingPage.zohoSignIn();
-		ZohoHomePO homePage = signInPage.signInWithEmailAndPassword("invaliduser1@gmail.com", "Avyaan@2023");
-		homePage.signOut();
-	}
-	
-//	@Test
-//	public void mytest4() throws Exception {
-//		
-//		this.getDriver().get("https://www.google.com");
-//		Thread.sleep(3000);
-//		this.getDriver().findElement(By.name("q1")).sendKeys("selenium");
-//		this.getDriver().findElement(By.name("q")).sendKeys(Keys.ENTER);
-//		Thread.sleep(5000);
-//
+//	@Test()
+//	public void mytest2() throws Exception {
+//		ZohoLogger logger = new ZohoLogger(this);
+//		ZohoLandingPO landingPage = new ZohoLandingPO(this.getDriver());
+//		logger.logInfo("Navigating to the landing Page...");
+//		landingPage.navigate();
+//		logger.logInfo("Clicking on the signin link");
+//		ZohoSignInPO signInPage = landingPage.zohoSignIn();
+//		ZohoHomePO homePage = signInPage.signInWithEmailAndPassword("hpamitsinha@gmail.com", "Avyaan@2023");
+//		homePage.signOut();
 //	}
-//
-//	@Test
-//	public void mytest5() throws Exception {
-//		this.getDriver().get("https://www.google.com");
-//		Thread.sleep(3000);
-//		this.getDriver().findElement(By.name("q")).sendKeys("testng");
-//		this.getDriver().findElement(By.name("q")).sendKeys(Keys.ENTER);
-//		Thread.sleep(5000);
-//
+//	
+//	@Test()
+//	public void mytest3() throws Exception {
+//		ZohoLogger logger = new ZohoLogger(this);
+//		ZohoLandingPO landingPage = new ZohoLandingPO(this.getDriver());
+//		logger.logInfo("Navigating to the landing Page...");
+//		landingPage.navigate();
+//		logger.logInfo("Clicking on the signin link");
+//		ZohoSignInPO signInPage = landingPage.zohoSignIn();
+//		ZohoHomePO homePage = signInPage.signInWithEmailAndPassword("invaliduser1@gmail.com", "Avyaan@2023");
+//		homePage.signOut();
 //	}
+	
+	@Test
+	public void mytest4() throws Exception {
+		
+		this.getDriver().get("https://www.google.com");
+		Thread.sleep(3000);
+		this.getDriver().findElement(By.name("q1")).sendKeys("selenium");
+		this.getDriver().findElement(By.name("q")).sendKeys(Keys.ENTER);
+		Thread.sleep(5000);
+
+	}
+
+	@Test
+	public void mytest5() throws Exception {
+		this.getDriver().get("https://www.google.com");
+		Thread.sleep(3000);
+		this.getDriver().findElement(By.name("q")).sendKeys("testng");
+		this.getDriver().findElement(By.name("q")).sendKeys(Keys.ENTER);
+		Thread.sleep(5000);
+
+	}
 }
